3d64be897b4a746267da23251ef7e75ad8925fa0
Ronny0113 <113224294+Ronny0113@users.noreply.github.com>
2022-10-10 00:41:05 +0500

    Интерфейс

--- PP3.py
+++ PP3.py
@@ -1,13 +1,48 @@
 print("Программа для зашифровки и расшифровки методом Цезаря на русском или английском языках")
 alpha1 = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
 alpha2 = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+
+
+def chek_eng(b):
+    for m in range(0, len(b)):
+        if b[m] not in alpha1:
+            return False
+    return True
+
+
+def chek_ru(b):
+    for m in range(0, len(b)):
+        if b[m] not in alpha1:
+            return False
+    return True
+
+
+def step_chek():
+    while True:
+        try:
+            b = int(input("Введите шаг шифровки: "))
+            return b
+        except ValueError:
+            print("Шаг должен быть циферками")
+
+
 while True:
-    a = int(input("Напишите 1 для зашифровки, напишите 2 для расшифровки, напишите 3 чтобы закончить: "))
+    try:
+        a = int(input("Напишите 1 для зашифровки, напишите 2 для расшифровки, напишите 3 чтобы закончить: "))
+    except ValueError:
+        print("Вы ввели не ту цифру")
+        continue
     if a == 1:
+        """while True:
+            word = input("Введиде слово для зашифровки: ")
+            if word.isalpha():
+                break
+            else:
+                print("Вы ввели неверное слово")"""
         word = input("Введиде слово для зашифровки: ")
         word = word.lower()
-        if word[0] in alpha1:
-            step = int(input("Введите шаг шифровки: "))
+        if chek_eng(word):
+            step = step_chek()
             if step > 25:
                 step %= 25
             newword = ""
@@ -16,8 +51,8 @@ while True:
                 newpos = pos + step
                 newword += alpha1[newpos]
             print("Ваше зашифрованное слово: " + newword)
-        elif word[0] in alpha2:
-            step = int(input("Введите шаг шифровки: "))
+        elif chek_ru(word):
+            step = step_chek()
             if step > 32:
                 step %= 32
             newword = ""
@@ -31,8 +66,8 @@ while True:
     elif a == 2:
         word = input("Введиде слово для расшифровки: ")
         word = word.lower()
-        if word[0] in alpha1:
-            step = int(input("Введите шаг шифровки: "))
+        if chek_eng(word):
+            step = step_chek()
             if step > 25:
                 step %= 25
             newword = ""
@@ -41,8 +76,8 @@ while True:
                 newpos = pos - step
                 newword += alpha1[newpos]
             print("Ваше расшифрованное слово: " + newword)
-        elif word[0] in alpha2:
-            step = int(input("Введите шаг шифровки: "))
+        elif chek_ru(word):
+            step = step_chek()
             if step > 32:
                 step %= 32
             newword = ""
